998393194932548eb2a1034e2f79d50add896048
Kagerou USB <f12c052jt08e163c@gmail.com>
2020-08-29 11:54:19 +0900

    first commit

---
+++ src/main/java/com/usb/usbtestapp/UsbTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.usb.usbtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsbTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsbTestAppApplication.class, args);
+	}
+
+}
